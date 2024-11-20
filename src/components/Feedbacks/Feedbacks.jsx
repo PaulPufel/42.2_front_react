bf9f8c8bc29fec48e2e197207063117897ad9d14
PaulPufel <ppufel@web.de>
2024-11-20 18:48:01 +0100

    Add homework04

---
+++ src/components/Feedbacks/Feedbacks.jsx
@@ -0,0 +1,29 @@
+import "./styles.css";
+import Button from "../Button/Button";
+import { useState } from "react";
+
+function Feedbacks() {
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+  
+    const clickLike = () => setLikes(likes + 1);
+    const clickDislike = () => setDislikes(dislikes + 1);
+    const resetResults = () => {
+      setLikes(0);
+      setDislikes(0);
+    };
+  
+    return (
+      <div className="feedbacks-container">
+        <div className="feedbacks-controls">
+          <span className="feedbacks-count">{likes}</span>
+          <Button name="Like" onClick={clickLike} />
+          <Button name="Dislike" onClick={clickDislike} />
+          <span className="feedbacks-count">{dislikes}</span>
+        </div>
+        <Button name="Reset Results" onClick={resetResults} />
+      </div>
+    );
+  }
+
+    export default Feedbacks;
