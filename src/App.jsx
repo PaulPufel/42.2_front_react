d47567a69358a4b8c0ee0ad60053953af4459b4f
PaulPufel <ppufel@web.de>
2024-11-20 13:10:00 +0100

    Add homework 02pp

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import "./App.css";
 //lesson imports
 // import Lesson02 from "./lessons/Lesson02/Lesson02"; 
 // import Lesson03 from "./lessons/Lesson03/Lesson03"; 
-// // import Lesson04 from "./lessons/Lesson04/Lesson04";
+// import Lesson04 from "./lessons/Lesson04/Lesson04";
 import Lesson05 from "./lessons/Lesson05/Lesson05";
 
 //homework imports
 // import Homework02 from "./homeworks/Homework02/Homework02"; 
+// import Homework02pp from "./homeworks/Homework02pp/Homework02pp";
 // import Homework03 from "./homeworks/Homework03/Homework03";
 // import Homework04cw from "./homeworks/Homework04cw/Homeworko4cw";
- import Homework03 from "./homeworks/Homework03/Homework03";
+
 
 //consultation imports
 // import Consult02 from "./consultation/Consult02/Consult02";
@@ -23,6 +24,7 @@ function App() {
       {/* <Lesson04 /> */}
       <Lesson05 />
       {/* <Homework02 /> */}
+      {/* <Homework02pp /> */}
       {/* <Homework03 /> */}
       {/* <Homework04cw /> */}
       {/* <Consult02 /> */}
