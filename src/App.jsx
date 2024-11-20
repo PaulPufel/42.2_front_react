34f06538856234df463e56c5aaa9e0c3701f06a4
PaulPufel <ppufel@web.de>
2024-11-20 12:07:04 +0100

    Add lesson 05

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 //lesson imports
 // import Lesson02 from "./lessons/Lesson02/Lesson02"; 
 // import Lesson03 from "./lessons/Lesson03/Lesson03"; 
-import Lesson04 from "./lessons/Lesson04/Lesson04";
+// import Lesson04 from "./lessons/Lesson04/Lesson04";
+import Lesson05 from "./lessons/Lesson05/Lesson05";
 
 //homework imports
 // import Homework02 from "./homeworks/Homework02/Homework02"; 
 // import Homework03 from "./homeworks/Homework03/Homework03";
+// import Homework04cw from "./homeworks/Homework04cw/Homeworko4cw";
 
 //consultation imports
 // import Consult02 from "./consultation/Consult02/Consult02";
@@ -16,11 +18,14 @@ function App() {
   return (
     <div className="App">
       {/* <Lesson02 /> */}
+      {/* <Lesson03 /> */}
+      {/* <Lesson04 /> */}
+      <Lesson05 />
       {/* <Homework02 /> */}
       {/* <Homework03 /> */}
-      {/* <Lesson03 /> */}
+      {/* <Homework04cw /> */}
       {/* <Consult02 /> */}
-      <Lesson04 />
+      
     </div>
   );
 }
---
+++ src/homeworks/Homework04cw/Homeworko4cw.jsx
@@ -0,0 +1,12 @@
+import Feedback from "../../components/Feedback/Feedback";
+import "./styles.css";
+
+function Homework04cw() {
+  return (
+    <div className="homework04-container">
+      <Feedback />
+    </div>
+  );
+}
+
+export default Homework04cw;
